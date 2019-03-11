29f301bc9375aba5497191907ca26e5b6d85f4f4
zhylmzr <zhylmzr@gmail.com>
2019-03-11 16:46:34 +0800

    add lint support

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-parameter-properties': 'off'
+  }
+};
